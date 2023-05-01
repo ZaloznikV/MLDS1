c19d7dc7c5cb1d39bc368c91655f5b01d8739b5e
ZaloznikV <zaloznik.vito@hotmail.com>
2023-05-01 20:15:19 +0200

    Added report and part3 - DGP process.

--- logistic_regression/logistic.py
+++ logistic_regression/logistic.py
@@ -7,7 +7,12 @@ Created on Tue Apr 25 15:44:21 2023
 from scipy.optimize import fmin_l_bfgs_b
 import pandas as pd
 import numpy as np
-data = pd.read_csv("dataset.csv", sep=";")
+from sklearn.model_selection import KFold
+import random
+from scipy.stats import sem
+from tqdm import tqdm
+
+
 from sklearn import preprocessing
 # print(data.columns)
 # print(data.dtypes)
@@ -24,13 +29,7 @@ def softmax(u):
 def inverse_logit(u):
     return 1/(1+np.exp(-u))
 
-one_hot_encoded_data = pd.get_dummies(data, columns =['Competition', 'PlayerType', 'Movement'])
-print(one_hot_encoded_data)
-X = one_hot_encoded_data.drop(columns=["ShotType","Angle", "Distance"])
-#normaliziraj kot in razdaljo, najbolj redek shot daj na konec kot reference value
-y = one_hot_encoded_data.ShotType
-label_encoder = preprocessing.LabelEncoder()
-y_encoded = label_encoder.fit_transform(y)
+
 
 
 class MultinomialLogReg():
@@ -104,9 +103,9 @@ class OrdinalLogReg():
         self.y = y
         self.n_classes = len(np.unique(self.y)) #all possible classes
         W = np.ones(self.X.shape[1]) #only vector of weights (not matrix)
-        diff = np.ones(self.n_classes-1) #vector of thresholds
+        diff = np.ones(self.n_classes-1) #vector of differences between thresholds
         bounds = len(W) * [(None, None)] + len(diff) * [(0, None)] # bounds for weights and differences between thresholds
-        bounds[len(W)]  = (None, None) #first difference is actual first threshold value and can be negative
+        bounds[len(W)]  = (None, None) #first difference is actual first threshold value and can be negative since we dont have intercept
         
         matrix = np.concatenate((W,diff)) #first weights, then differences
         
@@ -172,21 +171,21 @@ class OrdinalLogReg():
         
 
 
-test_X = np.array([[0, 0],
-                    [0, 1],
-                    [1, 0],
-                    [1, 1],
-                    [1, 1],
-                  [0, 0],
-                                      [0, 1],
-                                      [1, 0],
-                                      [1, 1],
-                                      [1, 1], [0, 1],
-                                      [1, 0],
-                                      [1, 1],
-                                      [1, 1]])
+# test_X = np.array([[0, 0],
+#                     [0, 1],
+#                     [1, 0],
+#                     [1, 1],
+#                     [1, 1],
+#                   [0, 0],
+#                                       [0, 1],
+#                                       [1, 0],
+#                                       [1, 1],
+#                                       [1, 1], [0, 1],
+#                                       [1, 0],
+#                                       [1, 1],
+#                                       [1, 1]])
 
-test_y = np.array([0, 0, 1, 1, 2, 0, 0, 1, 1, 2, 0, 1, 1, 2])
+# test_y = np.array([0, 0, 1, 1, 2, 0, 0, 1, 1, 2, 0, 1, 1, 2])
 
 
 # train_x, train_y = test_X[::2], test_y[::2] 
@@ -208,18 +207,237 @@ test_y = np.array([0, 0, 1, 1, 2, 0, 0, 1, 1, 2, 0, 1, 1, 2])
 # M = MultinomialLogReg()
 # M.build(X[:10],y_encoded[:10]) 
 
-#testing
+# #testing
 import time
-a = time.time()       
-L = MultinomialLogReg()
-M = L.build(X, y_encoded)
-print(time.time() - a)
-#print(np.argmax(L.predict(test_X), axis=1))
-print("test set accuracy: ", np.sum(y_encoded  == np.argmax(M.predict(X), axis=1))/len(y_encoded))
+     
+# L = MultinomialLogReg()
+# M = L.build(X, y_encoded)
+# print(time.time() - a)
+# #print(np.argmax(L.predict(test_X), axis=1))
+# print("test set accuracy: ", np.sum(y_encoded  == np.argmax(M.predict(X), axis=1))/len(y_encoded))
         
-L = OrdinalLogReg()
-L.build(test_X, test_y)
-print(np.sum(np.argmax(L.predict(test_X), axis=1) == test_y)/len(test_y))
+# L = OrdinalLogReg()
+# L.build(test_X, test_y)
+# print(np.sum(np.argmax(L.predict(test_X), axis=1) == test_y)/len(test_y))
 
- 
- 
+
+
+
+#evaluate models with cross validation
+
+# L = MultinomialLogReg()
+# M = L.build(X, y_encoded)
+# #print(time.time() - a)
+# #print(np.argmax(L.predict(test_X), axis=1))
+# print("test set accuracy: ", np.sum(y_encoded  == np.argmax(M.predict(X), axis=1))/len(y_encoded))
+        
+data = pd.read_csv("dataset.csv", sep=";")
+one_hot_encoded_data = pd.get_dummies(data, columns =['Competition', 'PlayerType', 'Movement'])
+y = one_hot_encoded_data.ShotType
+print("majority classifier: ", 3055/len(y))
+
+
+        
+data = pd.read_csv("dataset.csv", sep=";")
+one_hot_encoded_data = pd.get_dummies(data, columns =['Competition', 'PlayerType', 'Movement'])
+X = one_hot_encoded_data.drop(columns=["ShotType"]) #"Angle", "Distance"   
+
+labels = {'dunk': 0, 'tip-in': 1, 'layup': 2, 'hook shot': 3, 'above head': 4, 'other': 5}
+
+
+
+y_encoded = y.replace(labels)
+
+# model = MultinomialLogReg()
+# model.build(X, y_encoded)
+
+
+idx = (random.choices( list(range(3) ), k=5) )  
+
+X_joined = X.copy()
+X_joined["y_encoded"] = y_encoded    
+
+
+df = X_joined.sample(len(X_joined), replace=True)
+df_y = df.y_encoded
+df_x = df.drop(columns="y_encoded")
+scaler = preprocessing.MinMaxScaler()
+# df_x = scaler.fit_transform(df_x)
+
+a = time.time()
+
+# model = MultinomialLogReg()
+# model.build(X.to_numpy()[:1000], y_encoded.to_numpy()[:1000])
+
+
+def bootstrap_weights(n):
+    
+    weights_dunk = []
+    weights_tipin = []
+    weights_layup = []
+    weights_hookshot = []
+    weights_above_head = []
+
+    for i in range(n):
+        print(i)
+        scaler = preprocessing.MinMaxScaler()
+        data = pd.read_csv("dataset.csv", sep=";")
+        one_hot_encoded_data = pd.get_dummies(data, columns =['Competition', 'PlayerType', 'Movement'])
+        y = one_hot_encoded_data.ShotType
+        X = one_hot_encoded_data.drop(columns=["ShotType"]) #"Angle", "Distance"   
+        
+        labels = {'dunk': 0, 'tip-in': 1, 'layup': 2, 'hook shot': 3, 'above head': 4, 'other': 5}
+        
+        
+        
+        y_encoded = y.replace(labels)
+    
+        X_np = X.to_numpy()
+        y_encoded_np = y_encoded.to_numpy()
+        
+        idx = random.choices(list(range(X_np.shape[0])), k =X_np.shape[0])
+        
+        x_boot = X_np[idx]
+        x_boot = scaler.fit_transform(x_boot) #only angle and distance
+        y_boot = y_encoded_np[idx]
+        
+        model = MultinomialLogReg()
+        model.build(x_boot, y_boot)
+        
+        weights_dunk.append(model.W[:,0])
+        weights_tipin.append(model.W[:,1])
+        weights_layup.append(model.W[:,2])
+        weights_hookshot.append(model.W[:,3]) 
+        weights_above_head.append(model.W[:,4]) 
+    
+    return [weights_dunk, weights_tipin, weights_layup, weights_hookshot, weights_above_head] #zadnji intercept, prvi transition
+
+
+import pickle
+
+def summary(calculate = False):
+    if calculate:
+        weights_list = bootstrap_weights(30)
+        
+        with open('weights_list.pickle', 'wb') as handle:
+            pickle.dump(weights_list, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+    
+    with open('weights_list.pickle', 'rb') as handle:
+        weights_list = pickle.load(handle)
+    
+    dunk = np.column_stack([row for row in weights_list[0]])
+    tipin = np.column_stack([row for row in weights_list[1]])
+    layup = np.column_stack([row for row in weights_list[2]])
+    hookshot = np.column_stack([row for row in weights_list[3]])
+    overhead = np.column_stack([row for row in weights_list[4]])
+    
+    print("dunk_mean: ", np.mean(dunk, axis=1), "dunk_SE: ", sem(dunk, axis=1))
+    print("")
+    print("tipin_mean: ", np.mean(tipin, axis=1), "tipin_SE: ", sem(tipin, axis=1))
+    print("")
+    print("layup_mean: ", np.mean(layup, axis=1), "layup_SE: ", sem(layup, axis=1))
+    print("")
+    
+    print("hookshot_mean: ", np.mean(hookshot, axis=1), "hookshot_SE: ", sem(hookshot, axis=1))
+    print("")
+    
+    print("overhead_mean: ", np.mean(overhead, axis=1), "overhead_SE: ", sem(overhead, axis=1))
+
+
+
+
+def multinomial_bad_ordinal_good(size, rand):
+    classes = np.random.choice([0, 1, 2], size=size, p=[0.05, 0.3, 0.65]) #not equally distributed between classes
+    gender = np.random.choice([0, 1], size=size)
+    income = np.random.normal(1, 1, size=size)
+    age = np.random.normal(0, 1, size=size)
+    data = pd.DataFrame({'Classes': classes, 'Age': age, 'Gender': gender, 'Income': income})
+    y = data.Classes.to_numpy()
+    
+    data = data.drop(columns=["Classes"])
+    X = data.to_numpy()
+    
+    return X,y
+
+MBOG_TRAIN = 150
+
+def log_loss(proba, true_y):
+    s = 0
+    for i in (range(len(true_y))):
+        s -= np.log(proba[i, true_y[i]])
+    
+    return s
+
+
+def log_loss_uncertaints(n):
+    multi_loss = []
+    ordinal_loss = []
+    for i in tqdm(range(n)):
+        X_train, y_train = multinomial_bad_ordinal_good(MBOG_TRAIN, random.Random(1))
+        
+        X_test, y_test = multinomial_bad_ordinal_good(1000, random.Random(1))
+        
+        M = MultinomialLogReg()
+        O = OrdinalLogReg()
+        
+        M.build(X_train, y_train)
+        O.build(X_train, y_train)
+        
+        multi_proba = M.predict(X_test)
+        ordinal_proba = O.predict(X_test)
+        
+        # print(log_loss(multi_proba, y_test))
+        # print(log_loss(ordinal_proba, y_test))
+        
+        multi_loss.append(log_loss(multi_proba, y_test))
+        ordinal_loss.append(log_loss(ordinal_proba, y_test))
+        
+    print(np.mean(multi_loss), np.mean(ordinal_loss))    
+    print(sem(multi_loss), sem(ordinal_loss))    
+
+    return multi_loss, ordinal_loss
+
+        
+        
+    
+    
+    
+    
+    
+
+        
+        
+        
+        
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
